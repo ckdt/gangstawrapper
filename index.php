acb49d81fe91efbdcc160869821b58d57aca9265
Ronny Wieckardt <ronny@ckdt.nl>
2015-05-28 08:59:56 +0200

    Added Favicon's

--- index.php
+++ index.php
@@ -3,12 +3,35 @@
 <head>
 		<meta charset="utf-8"/>
 		<title>Ronny Wieckardt &mdash; Web / App &amp; Interaction Design &mdash; Rotterdam, The Netherlands</title>
-		
-		<meta name="description" content="">
-		<meta name="author" content="Ronny Wieckardt">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Online portfolio of Ronny Wieckardt. Interactive Designer based in Rotterdam, The Netherlands.">
+		<meta name="author" content="Ronny Wieckardt">
+		
+		<meta property="og:image" content="http://ronnywieckardt.nl/img/fav_icon_512.gif"/> 
+		<meta property="og:title" content="Ronny Wieckardt &mdash; Web / App &amp; Interaction Design &mdash; Rotterdam, The Netherlands."/>
+		<meta property="og:url" content="http://ronnywieckardt.nl"/>
+		<meta property="og:site_name" content="Online portfolio of Ronny Wieckardt."/>
+		<meta property="og:type" content="website"/>
 
+		<link rel="apple-touch-icon" sizes="57x57" href="img/favicons/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="img/favicons/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="img/favicons/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="img/favicons/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="img/favicons/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/favicons/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="img/favicons/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="img/favicons/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="img/favicons/android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-16x16.png" sizes="16x16">
 		<link rel="shortcut icon" href="favicon.ico">
+		<link rel="manifest" href="img/favicons/manifest.json">
+		<meta name="msapplication-TileColor" content="#c09c62">
+		<meta name="msapplication-TileImage" content="img/favicons/mstile-144x144.png">
+		<meta name="theme-color" content="#c09c62">
+
 		<link rel="stylesheet" href="css/materialize.min.css" />
 		<link rel="stylesheet" href="css/style.css" />
 
